2d6ec902bcd4d22cd3e4d07e3e4dbed679f746ee
MartinACarrion <martin.a.carrion-1@ou.edu>
2019-01-18 13:52:22 -0600

    YAY

--- SolutionForReview/SolutionForReview/Program.cs
+++ SolutionForReview/SolutionForReview/Program.cs
@@ -10,29 +10,47 @@ namespace SolutionForReview
     {
         static void Main(string[] args)
         {
-            int PNumber;
-            int SNumber;
+            //int PNumber;
+            //int SNumber;
 
-            Console.WriteLine("Enter a Number Between 1 and 10");
-            PNumber=Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter a Number Between 1 and 10");
-            SNumber = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("So You Want To Multiply " + PNumber + " , " + SNumber + "Yes or No");
-            string Answer=Console.ReadLine();
+            //Console.WriteLine("Enter a Number Between 1 and 10");
+            //PNumber=Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter a Number Between 1 and 10");
+            //SNumber = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("So You Want To Multiply " + PNumber + " , " + SNumber + "Yes or No");
+            //string Answer=Console.ReadLine();
 
-            if (Answer=="Yes")
-            {
-                Console.WriteLine("The Answer is " + PNumber * SNumber);
-            }
-            else if (Answer=="No")
-            {
-                Console.WriteLine("Then Why Did You Answer the 1st Two Questions Wrong!!!");
+            //if (Answer=="Yes")
+            //{
+            //    Console.WriteLine("The Answer is " + PNumber * SNumber);
+            //}
+            //else if (Answer=="No")
+            //{
+            //    Console.WriteLine("Then Why Did You Answer the 1st Two Questions Wrong!!!");
 
-            }
-            else
-            {
-                Console.WriteLine("Press Any Key to ESCAPE");
-            }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Press Any Key to ESCAPE");
+            //}
+
+            string x = "Squirrel";
+            Console.WriteLine(x);
+            int xyz = 5;
+            Console.WriteLine(xyz);
+
+            bool y = true;
+            Console.WriteLine(y);
+
+            double z = 3.5;
+            int k = 2;
+            Console.WriteLine(z * k);
+
+            
+
+            
+
+
             
            
 
