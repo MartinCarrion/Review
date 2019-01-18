aeff83b368d6bdffc1de53940ce13cd25536e631
MartinACarrion <martin.a.carrion-1@ou.edu>
2019-01-18 13:10:56 -0600

    a little bit of Review

---
+++ SolutionForReview/SolutionForReview/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolutionForReview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int PNumber;
+            int SNumber;
+
+            Console.WriteLine("Enter a Number Between 1 and 10");
+            PNumber=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a Number Between 1 and 10");
+            SNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("So You Want To Multiply " + PNumber + " , " + SNumber + "Yes or No");
+            string Answer=Console.ReadLine();
+
+            if (Answer=="Yes")
+            {
+                Console.WriteLine("The Answer is " + PNumber * SNumber);
+            }
+            else if (Answer=="No")
+            {
+                Console.WriteLine("Then Why Did You Answer the 1st Two Questions Wrong!!!");
+
+            }
+            else
+            {
+                Console.WriteLine("Press Any Key to ESCAPE");
+            }
+            
+           
+
+
+
+
+
+
+            Console.ReadKey();
+        }//Dont Write Below This Line
+    }
+}
